e3614c78d6039585695a77cf2a36f3db6267e753
Kamiya <kamiya@kamiya.app>
2024-12-20 09:17:12 +0800

    fix: footer layout

--- src/components/app/footer.tsx
+++ src/components/app/footer.tsx
@@ -1,12 +1,24 @@
 'use client';
 
 import { Computer, Moon, Sun } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useTheme } from 'next-themes';
 
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
+
+const ThemeIcons = {
+  dark: <Moon />,
+  light: <Sun />,
+  system: <Computer />,
+} as Record<string, React.ReactNode>;
+
+const ThemeNames = {
+  dark: '深色',
+  light: '淺色',
+  system: '系統',
+} as Record<string, string>;
 
 export default function AppFooter() {
   const { setTheme, theme, themes } = useTheme();
@@ -22,16 +34,16 @@ export default function AppFooter() {
 
   return (
     <footer className={`
-      mt-12 flex w-full flex-col items-center justify-between border-t
-      bg-muted/40 px-4 py-4 text-muted-foreground
+      mt-12 flex w-full flex-col items-center justify-between gap-2 border-t
+      bg-muted/40 px-4 py-6 text-muted-foreground
       lg:px-16
       md:flex-row md:px-8 md:py-8
       xl:px-32
     `}
     >
       <div className={`
-        flex flex-col items-center gap-6
-        md:flex-row
+        flex flex-col items-center gap-3
+        md:flex-row md:gap-8
       `}
       >
         <span>狄西機霸 &copy; 2024</span>
@@ -44,25 +56,24 @@ export default function AppFooter() {
       <div>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="outline" size="icon">
-              {
-                {
-                  dark: <Moon />,
-                  light: <Sun />,
-                  system: <Computer />,
-                }[theme ?? 'system']
-              }
+            <Button variant="ghost">
+              {ThemeIcons[theme ?? 'system']}
+              {ThemeNames[theme ?? 'system']}
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent>
-            {...themes.map((t) => (
-              <DropdownMenuItem
-                key={t}
-                onClick={() => setTheme(t)}
-              >
-                {t}
-              </DropdownMenuItem>
-            ))}
+            <DropdownMenuItem onClick={() => setTheme('light')}>
+              <Sun />
+              <span>淺色</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme('dark')}>
+              <Moon />
+              <span>深色</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme('system')}>
+              <Computer />
+              <span>系統</span>
+            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
